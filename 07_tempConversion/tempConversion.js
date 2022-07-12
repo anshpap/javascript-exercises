fa5e44d74411bfbf7e18bcc56b47c4db3185fa29
Anshul Papneja <anshulpapneja@outlook.com>
2022-07-12 15:53:41 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  return +tempC.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * 9 / 5) + 32;
+  return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
