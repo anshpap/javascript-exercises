7d669d7949bce4a7850813d1510da5e07f37286f
Anshul Papneja <anshulpapneja@outlook.com>
2022-07-12 15:45:36 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    /* initial approach (looks cleaner tbh)
+    
+    if (year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    if (year % 4 === 0) return true;
+    return false;
+    */
 
+    return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 };
 
 // Do not edit below this line
